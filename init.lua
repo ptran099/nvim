71ce899b6caeec59526faa64d5f9c9ab4c4cd992
ptran099 <6279104+ptran099@users.noreply.github.com>
2026-01-12 13:27:56 -0500

    update config filenames

--- init.lua
+++ init.lua
@@ -4,17 +4,13 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 
 Plug('ellisonleao/gruvbox.nvim')
-Plug('junegunn/seoul256.vim')
 Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
-Plug('nvim-treesitter/nvim-treesitter')
 
 vim.call('plug#end')
 
 vim.o.background = "dark"
 vim.cmd('colorscheme gruvbox')
-vim.cmd('set tabstop=4')
-vim.cmd('set shiftwidth=4')
 
 local cmp = require('cmp')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
@@ -30,28 +26,21 @@ cmp.setup({
 -- === LSP Setup (built-in API, no nvim-lspconfig) ===
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-local clangd_config = {
-  name = 'clangd',
+vim.lsp.config['clangd'] = {
   cmd = {
     'clangd',
     '--header-insertion=never',
     '--query-driver=/usr/bin/clang*',
+    '--include-directory=/usr/include/SDL2',
   },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
-  root_dir = vim.fs.dirname(vim.fs.find({'.git', 'compile_commands.json'}, { upward = true })[1] or vim.api.nvim_buf_get_name(0)),
   capabilities = capabilities,
 }
 
--- Automatically start clangd for C/C++ files, only if not already running
+-- Automatically start clangd for C/C++ files
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'c', 'cpp', 'objc', 'objcpp' },
   callback = function()
-    local clients = vim.lsp.get_clients({bufnr = bufnr,})
-    for _, client in pairs(clients) do
-      if client.name == 'clangd' then
-        return
-      end
-    end
-    vim.lsp.start(clangd_config)
+    vim.lsp.start(vim.lsp.config['clangd'])
   end,
 })
--- t48b_init.lua
+++ t48b_init.lua
@@ -4,13 +4,17 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 
 Plug('ellisonleao/gruvbox.nvim')
+Plug('junegunn/seoul256.vim')
 Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
+Plug('nvim-treesitter/nvim-treesitter')
 
 vim.call('plug#end')
 
 vim.o.background = "dark"
 vim.cmd('colorscheme gruvbox')
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4')
 
 local cmp = require('cmp')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
@@ -26,21 +30,28 @@ cmp.setup({
 -- === LSP Setup (built-in API, no nvim-lspconfig) ===
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-vim.lsp.config['clangd'] = {
+local clangd_config = {
+  name = 'clangd',
   cmd = {
     'clangd',
     '--header-insertion=never',
     '--query-driver=/usr/bin/clang*',
-    '--include-directory=/usr/include/SDL2',
   },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
+  root_dir = vim.fs.dirname(vim.fs.find({'.git', 'compile_commands.json'}, { upward = true })[1] or vim.api.nvim_buf_get_name(0)),
   capabilities = capabilities,
 }
 
--- Automatically start clangd for C/C++ files
+-- Automatically start clangd for C/C++ files, only if not already running
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'c', 'cpp', 'objc', 'objcpp' },
   callback = function()
-    vim.lsp.start(vim.lsp.config['clangd'])
+    local clients = vim.lsp.get_clients({bufnr = bufnr,})
+    for _, client in pairs(clients) do
+      if client.name == 'clangd' then
+        return
+      end
+    end
+    vim.lsp.start(clangd_config)
   end,
 })
