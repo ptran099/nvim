64156544359ab3e74b48a54fb737f268f24e4ec0
ptran099 <6279104+ptran099@users.noreply.github.com>
2026-06-09 02:34:00 -0400

    update t48c init

--- t48c_init.lua
+++ t48c_init.lua
@@ -1,6 +1,7 @@
 --[[
 -- Peter Tran's Definitive Neovim Configuration File
 -- Tested for: NVIM v0.12.1
+-- [:] + [tab] drop-down is native NVIM feature
 -- ]]
 local vim = vim
 local plug = vim.fn['plug#']
@@ -10,8 +11,14 @@ local plug = vim.fn['plug#']
 -- ]]
 vim.call('plug#begin')
 plug('ellisonleao/gruvbox.nvim') -- default true-color mode
-plug('joshdick/onedark.vim') -- default 256-color mode
+plug('joshdick/onedark.vim') -- default 256-color mode (macos no true-color support)
+plug('hrsh7th/nvim-cmp') -- completion engine, completion engine needs snippet engine
+plug('L3MON4D3/LuaSnip') -- snippet engine, snippet engine needs snippet sources
 vim.call('plug#end')
+
+--[[
+-- Plugin settings
+-- ]]
 
 --[[
 -- Commands
